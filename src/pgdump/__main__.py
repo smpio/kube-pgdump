2b7b01aafd55d32418b1d460ee3d766548f9898e
Nikita Kurinnyi <f1aky927@gmail.com>
2017-07-12 18:23:00 +0300

    added transfer logic

--- src/pgdump/__main__.py
+++ src/pgdump/__main__.py
@@ -2,46 +2,109 @@ import datetime
 import logging
 
 from pgdump.client import Client
+from pgdump import config
 
-logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
 
 
 def main():
+    logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.NOTSET)
     client = Client()
     for pod in client.pods_for_dump():
         name = pod.metadata.name
         namespace = pod.metadata.namespace
-        container_name = pod.spec.containers[0].name
+        template_hash = pod.metadata.labels.get('pod-template-hash')
+        if template_hash and template_hash in name:
+            display_name = name.split(f'-{template_hash}', 1)[0]
+        else:
+            display_name = name
 
-        logging.info('processing pod %s/%s', namespace, name)
+        log.debug('processing pod %s/%s', namespace, name)
 
         try:
-            logging.info('fetch pvc ...')
+            log.debug('fetch pvc ...')
             pvc_name = pod.spec.volumes[0].persistent_volume_claim.claim_name
-            logging.info('fount %s pvc', pvc_name)
+            log.debug('fount %s pvc', pvc_name)
         except IndexError:
-            logging.error('Not found volumes[pod %s/%s]', namespace, name)
+            log.error('Not found volumes[pod %s/%s]', namespace, name)
             continue
         except AttributeError:
-            logging.error('Invalid volume type[pod %s/%s]', namespace, name)
+            log.error('Invalid volume type[pod %s/%s]', namespace, name)
             continue
 
         # TODO check postgres version
 
-        logging.info('prepare to make dump')
+        log.debug('prepare to make dump')
 
         now = datetime.datetime.utcnow()
-        today_str = now.strftime('%d.%m.%y')
-        dump_filename = f'{container_name}.{today_str}.tar.gz'
+        now_str = now.strftime('%d.%m.%y-%H:%M:%S')
+        dump_filename = f'{now_str}.tar.gz'
 
         try:
-            logging.info('make dump ....')
+            log.debug('make dump ....')
             client.make_dump(name, namespace, filename=dump_filename)
         except Exception:
-            logging.exception('make dump problem [pod %s/%s]', namespace, name)
+            log.exception('make dump problem [pod %s/%s]', namespace, name)
             continue
 
-        logging.info('dump %s complete', dump_filename)
+        log.debug('dump %s complete', dump_filename)
+
+        log.debug('create transfer pod')
+
+        mount_path = '/backup'
+        backup_pod_name = f'{display_name}-backup'
+
+        client.create_pod({
+            'apiVersion': 'v1',
+            'kind': 'Pod',
+            'metadata': {
+                'name': backup_pod_name,
+                'namespace': namespace
+            },
+            'spec': {
+                'containers': [
+                    {
+                        'name': 'main',
+                        'image': config.ftp_image,
+                        'imagePullPolicy': 'Always',
+                        'env': [
+                            {'name': 'FTP_SERVER', 'value': config.ftp_server},
+                            {'name': 'FTP_LOGIN', 'value': config.ftp_login},
+                            {'name': 'FTP_PASSWORD', 'value': config.ftp_password},
+                            {'name': 'FTP_WORKDIR', 'value': f'/{namespace}/{display_name}'},
+                            {'name': 'MAX_BACKUP_COUNT', 'value': config.max_backup_count},
+                            {'name': 'DUMP_FILENAME', 'value': f'{mount_path}/{dump_filename}'},
+                            {'name': 'FTP_NET_LIMIT', 'value': config.ftp_speed_limit},
+                        ],
+                        'volumeMounts': [
+                            {
+                                'mountPath': mount_path,
+                                'name': 'backup-data',
+                            }
+                        ]
+                    },
+                ],
+                'restartPolicy': 'Never',
+                'volumes': [
+                    {
+                        'name': 'backup-data',
+                        'persistentVolumeClaim': {
+                            'claimName': pvc_name
+                        }
+                     },
+                ]
+            },
+        })
+
+        client.wait_for_pod(backup_pod_name, namespace)
+        log.debug('fetch log')
+        pod_logs = client.get_pod_log(backup_pod_name, namespace)
+        if 'copy dump succes' in pod_logs:
+            log.info('copy dump success')
+        else:
+            log.error(pod_logs)
+
+        client.delete_pod(backup_pod_name, namespace)
 
 
 if __name__ == '__main__':
--- src/pgdump/config.py
+++ src/pgdump/config.py
@@ -7,9 +7,10 @@ pod_label_selector = 'pgdump=true'
 dump_command = 'pg_dump --username={POSTGRES_USER} --dbname={POSTGRES_DB} --format=t --clean ' \
                '--file={PGDATA}/{filename}'
 
-ftp_image = 'flaky/k8s-ftp-backup:2'
+ftp_image = 'flaky/k8s-ftp-backup:4'
 ftp_server = os.environ['FTP_SERVER']
 ftp_login = os.environ['FTP_LOGIN']
 ftp_password = os.environ['FTP_PASSWORD']
+ftp_speed_limit = '60M:60M'
 
 max_backup_count = os.environ['MAX_BACKUP_COUNT']
