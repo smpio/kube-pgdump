cc4cfc94671b13117adab7ce14a0a9d2d08b4454
Nikita Kurinnyi <f1aky927@gmail.com>
2017-07-12 18:22:44 +0300

    update transfer code

--- src/backup_transfer/ftp_transfer.sh
+++ src/backup_transfer/ftp_transfer.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 
-set -e
+set -ex
 
 function run_lftp {
     lftp -u ${FTP_LOGIN},${FTP_PASSWORD} -e "set ftp:ssl-allow no;${1}quit;" ${FTP_SERVER} ${@:2}
 }
+
+run_lftp "mkdir -fp ${FTP_WORKDIR};" || true
+
 
 while [ "$(run_lftp "cd ${FTP_WORKDIR};ls -tr;" | grep "tar\.gz$" | wc -l)" -ge "${MAX_BACKUP_COUNT}" ]; do
     filename=$(run_lftp "cd ${FTP_WORKDIR};ls -1qtr;" | grep "tar\.gz$" | head -1);
     run_lftp "rm ${FTP_WORKDIR}/${filename};";
 done
 
-run_lftp "cd ${FTP_WORKDIR}/;put ${DUMP_FILENAME};"
+run_lftp "set net:limit-total-rate ${FTP_NET_LIMIT};cd ${FTP_WORKDIR}/;put ${DUMP_FILENAME};"
 
 echo -e "copy dump success"
+
+rm -f ${DUMP_FILENAME}
